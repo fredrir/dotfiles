823f048563faf31fa41d1257b6be13c85c5e4b61
fredrir <fhansteen@gmail.com>
2026-03-21 16:09:29 +0100

    Add aliases to zsh

--- zsh/conf.d/10-env.zsh
+++ zsh/conf.d/10-env.zsh
@@ -1,3 +1,7 @@
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
 export NVM_DIR="$HOME/.config/nvm"
 export LESS="-R -F -X"
+
+export CONFIG="$HOME/.config"
+export HYPR="$CONFIG/hypr"
+export NVIM="$CONFIG/nvim"
--- zsh/conf.d/40-aliases.zsh
+++ zsh/conf.d/40-aliases.zsh
@@ -1,2 +1,33 @@
+alias n="nvim"
+alias nn="nvim ."
 alias v="nvim"
 alias vv="nvim ."
+
+alias la="ls -a"
+alias ll="ls -l"
+
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias f='find . -type f -name'
+# Git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gp='git push'
+alias gl='git pull'
+alias gd='git diff'
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias cdh="cd $HOME"
+alias cdc="cd $CONFIG"
+alias cdhy="cd $HYPR"
+alias cdn="cd $NVIM"
