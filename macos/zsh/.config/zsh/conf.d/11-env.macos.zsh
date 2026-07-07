614b62a8b500bf55a2b46f76914211af06a2292a
fredrir <fhansteen@gmail.com>
2026-07-07 08:40:08 +0000

    fix(macos): enable starship prompt setup

--- macos/zsh/.config/zsh/conf.d/11-env.macos.zsh
+++ macos/zsh/.config/zsh/conf.d/11-env.macos.zsh
@@ -3,5 +3,10 @@
 # oh-my-zsh custom dir ($ZSH is set in 05-ohmyzsh.zsh).
 export ZSH_CUSTOM="$ZSH/custom"
 
-# Homebrew (Apple Silicon). Sets PATH/MANPATH/etc.
-[ -x /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+# Homebrew. Sets PATH/MANPATH/etc before later fragments look up tools like
+# starship.
+if [ -x /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [ -x /usr/local/bin/brew ]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
