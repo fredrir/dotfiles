28bb94bf70b7ec04b8a9a424229800f5053e8e57
fredrir <fhansteen@gmail.com>
2026-08-28 22:54:06 +0200

    expand lua keymap

--- shared/wezterm/keymap/init.lua
+++ shared/wezterm/keymap/init.lua
@@ -1,20 +1,26 @@
 local wezterm = require "wezterm" ---@type Wezterm
 local platform = require "utils.platform"
-local act = wezterm.action
 local resize_window = require "utils.resize-window"
+local bind_keys = require "utils.bind-keys"
+local act = wezterm.action
 
 local M = {}
 
 if platform.is_mac then
   M.SUPER = "CMD"
-  M.MOD = "CMD"
   M.ALT = "OPT"
-  M.MUX = "CMD|SHIFT"
-elseif platform.is_win or platform.is_linux then
+  M.SPECIAL = "CMD"
+  M.SPECIAL_OR_CTRL = { "CMD", "CTRL" }
+  --
+  M.SPLITSIDE = "'"
+else
   M.SUPER = "CTRL"
-  M.MOD = "Alt"
   M.ALT = "ALT"
-  M.MUX = "ALT|SHIFT"
+  M.SPECIAL = "ALT"
+  M.SPECIAL_OR_CTRL = { "ALT", "CTRL" }
+  --
+  M.SPLITSIDE = "§"
+  --
 end
 
 M.attach = wezterm.action_callback(function(_window, pane)
@@ -22,12 +28,45 @@ M.attach = wezterm.action_callback(function(_window, pane)
 end)
 
 ---@type Key[]
-local keys = {
+local keys = bind_keys {
   { key = "c", mods = M.SUPER, action = act.CopyTo "Clipboard" },
   { key = "v", mods = M.SUPER, action = act.PasteFrom "Clipboard" },
   { key = "n", mods = M.SUPER, action = act.SpawnWindow },
 
   -- Window Management --
+  { -- Detach from session
+    key = "d",
+    mods = M.SPECIAL,
+    action = act.DetachDomain "CurrentPaneDomain",
+  },
+  { -- New Tab --
+    key = "t",
+    mods = M.SPECIAL_OR_CTRL,
+    action = act.SpawnTab "CurrentPaneDomain",
+  },
+  { -- Quit Window --
+    key = "q",
+    mods = M.SPECIAL,
+    action = act.QuitApplication,
+  },
+  { -- Quit Tab --
+    key = "w",
+    mods = M.SPECIAL,
+    action = act.CloseCurrentTab { confirm = false },
+  },
+  { -- Go to last tab --
+    key = "0",
+    mods = M.SPECIAL,
+    action = act.ActivateTab(-1),
+  },
+
+  { -- Splt Pane --
+    key = M.SPLITSIDE,
+    mods = "CMD",
+    action = act.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+
+  -- Zoom Controls --
   { key = "-", mods = M.SUPER, action = resize_window(-50) },
   { key = "+", mods = M.SUPER, action = resize_window(50) },
 
@@ -35,26 +74,34 @@ local keys = {
   { key = ".", mods = M.SUPER, action = M.attach },
 }
 
----@type Key[]
-local mac_cursor_motion_keys = {
-  { key = "LeftArrow", mods = "ALT", action = act.SendKey { key = "b", mods = "ALT" } },
-  { key = "RightArrow", mods = "ALT", action = act.SendKey { key = "f", mods = "ALT" } },
-  { key = "LeftArrow", mods = "CMD", action = act.SendKey { key = "a", mods = "CTRL" } },
-  { key = "RightArrow", mods = "CMD", action = act.SendKey { key = "e", mods = "CTRL" } },
-}
+-- Go to tab 1..9
+for i = 1, 9 do
+  table.insert(keys, {
+    key = tostring(i),
+    mods = M.SPECIAL,
+    action = act.ActivateTab(i - 1),
+  })
+end
 
--- if platform.is_mac then
---   for _, key in ipairs(mac_cursor_motion_keys) do
---     table.insert(keys, key)
---   end
--- end
+if platform.is_mac then
+  ---@type Key[]
+  local cursor_motion_keys = {
+    { key = "LeftArrow", mods = M.ALT, action = act.SendKey { key = "b", mods = "ALT" } },
+    { key = "RightArrow", mods = M.ALT, action = act.SendKey { key = "f", mods = "ALT" } },
+    { key = "LeftArrow", mods = M.SUPER, action = act.SendKey { key = "a", mods = "CTRL" } },
+    { key = "RightArrow", mods = M.SUPER, action = act.SendKey { key = "e", mods = "CTRL" } },
+  }
 
----@type Config
-return {
-  disable_default_key_bindings = true,
-  -- disable_default_mouse_bindings = true,
-  leader = { key = "SHIFT", mods = M.MOD },
-  keys = keys,
-  -- key_tables = key_tables,
-  -- mouse_bindings = mouse_bindings,
-}
+  table.move(cursor_motion_keys, 1, #cursor_motion_keys, #keys + 1, keys)
+end
+
+function M.apply_to_config(config)
+  config.disable_default_key_bindings = true
+  -- config.disable_default_mouse_bindings = true
+  -- config.leader = {}
+  config.keys = keys
+  -- config.key_tables = key_tables
+  -- config.mouse_bindings = mouse_bindings
+end
+
+return M
---
+++ shared/wezterm/utils/bind-keys.lua
@@ -0,0 +1,31 @@
+---@class BindKey
+---@field key string
+---@field mods string|string[]
+---@field action Action
+
+---@param bindings BindKey[]
+---@return Key[]
+local function bind_keys(bindings)
+  ---@type Key[]
+  local keys = {}
+
+  for _, binding in ipairs(bindings) do
+    local mods = binding.mods
+
+    if type(mods) == "string" then
+      mods = { mods }
+    end
+
+    for _, mod in ipairs(mods) do
+      table.insert(keys, {
+        key = binding.key,
+        mods = mod,
+        action = binding.action,
+      })
+    end
+  end
+
+  return keys
+end
+
+return bind_keys
