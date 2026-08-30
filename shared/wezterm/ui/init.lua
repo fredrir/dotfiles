6884354fc54dd28deb1c7750faefb927fa098bb5
fredrir <fhansteen@gmail.com>
2026-08-30 09:48:36 +0200

    wez: tweak max fps

--- shared/wezterm/ui/init.lua
+++ shared/wezterm/ui/init.lua
@@ -1,9 +1,9 @@
-local max_fps = require "ui.performance"
+local performance = require "ui.performance"
 local gpu_adapters = require "utils.gpu-adapter"
 
 ---@type Config
 local settings = {
-  max_fps = max_fps,
+  max_fps = performance.max_fps,
   webgpu_preferred_adapter = gpu_adapters:pick_best(),
 }
 
--- shared/wezterm/ui/performance.lua
+++ shared/wezterm/ui/performance.lua
@@ -1,12 +1,6 @@
 local platform = require "utils.platform"
 
----@type number|nil
-local max_fps
-
-if platform.is_mac then
-  max_fps = 120
-else
-  max_fps = nil
-end
-
-return max_fps
+---@type { max_fps: number? }
+return {
+  max_fps = platform.is_mac and 120 or nil,
+}
