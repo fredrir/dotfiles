8a5c0eb4f1ff01b3aa5a2fa2477ce40f390066a5
fredrir <fhansteen@gmail.com>
2026-09-01 05:18:20 +0200

    .

--- shared/wezterm/keymap/motion-keys.lua
+++ shared/wezterm/keymap/motion-keys.lua
@@ -6,8 +6,8 @@ local act = wezterm.action
 
 ---@type KeySpec[]
 local motion_keys = {
-  { key = "LeftArrow", mods = MOD.SECONDARY, action = act.SendKey { key = "b", mods = "ALT" } },
-  { key = "RightArrow", mods = MOD.SECONDARY, action = act.SendKey { key = "f", mods = "ALT" } },
+  { key = "LeftArrow", mods = MOD.UNIQUE, action = act.SendKey { key = "b", mods = "ALT" } },
+  { key = "RightArrow", mods = MOD.UNIQUE, action = act.SendKey { key = "f", mods = "ALT" } },
   { key = "LeftArrow", mods = MOD.PRIMARY, action = act.SendKey { key = "a", mods = "CTRL" } },
   { key = "RightArrow", mods = MOD.PRIMARY, action = act.SendKey { key = "e", mods = "CTRL" } },
 }
