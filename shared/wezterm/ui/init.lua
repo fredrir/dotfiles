36e7a2ede2f0573614c82a4929b9c65660a67abe
fredrir <fhansteen@gmail.com>
2026-09-01 05:58:34 +0200

    .

--- shared/wezterm/ui/init.lua
+++ shared/wezterm/ui/init.lua
@@ -9,15 +9,20 @@ local ui_config = {
   font = wezterm.font_with_fallback { { family = fonts.nerd_family, weight = "Medium" } },
   font_size = fonts.font_size,
 
+  -- tab bar
+  hide_tab_bar_if_only_one_tab = false,
+
+  -- window
   window_frame = {
     font = wezterm.font_with_fallback { { family = fonts.general_family } },
     font_size = fonts.interface_font_size,
   },
+  window_close_confirmation = "NeverPrompt",
   inactive_pane_hsb = {
-    hue = 1,
-    saturation = 1.0,
-    brightness = 1.0,
+    saturation = 1,
+    brightness = 1,
   },
+
   colors = profiles.active.colors,
   color_schemes = profiles.profiles,
   max_fps = performance.max_fps,
