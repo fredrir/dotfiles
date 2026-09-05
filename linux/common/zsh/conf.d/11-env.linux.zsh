60abca0d57c49bf700f7b6a94ee6624bd21b9ad9
fredrir <fhansteen@gmail.com>
2026-09-05 02:54:49 +0200

    fix 11-env.linux.zsh

--- linux/common/zsh/conf.d/11-env.linux.zsh
+++ linux/common/zsh/conf.d/11-env.linux.zsh
@@ -1,3 +1,11 @@
+if [[ -z ${XDG_RUNTIME_DIR:-} && -d /run/user/$EUID && -O /run/user/$EUID ]]; then
+  export XDG_RUNTIME_DIR="/run/user/$EUID"
+fi
+
+if [[ -z ${DBUS_SESSION_BUS_ADDRESS:-} && -S ${XDG_RUNTIME_DIR:-}/bus ]]; then
+  export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+fi
+
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
 # Test
 export SUDO_EDITOR="/usr/bin/nvim"
