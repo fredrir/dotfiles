e75b7cb8c0174813b5ba4e7cf6f25f7c48292e25
fredrir <fhansteen@gmail.com>
2026-09-22 15:52:53 +0200

    .

--- shared/zsh/31-projects.zsh
+++ shared/zsh/31-projects.zsh
@@ -2,7 +2,12 @@
 alias cdi="cd $HOME/infra"
 alias cdpw="cd $HOME/projects/wez-plugins/vertical-tabs"
 alias cdwp="cd $HOME/projects/wez-plugins/vertical-tabs"
-alias cdl="cd $HOME/llunde"
+
+export LLUNDE="$HOME/llunde"
+alias cdl="cd  $LLUNDE"
+alias cdlp="cd $LLUNDE/pyparser"
+alias cdlf="cd $LLUNDE/frontend"
+alias cdlb="cd $LLUNDE/backend"
 
 alias ww="just --justfile $HOME/projects/wez-plugins/vertical-tabs/justfile"
 alias wwr="just --justfile $HOME/projects/wez-plugins/vertical-tabs/justfile restart"
